d8a06f9ed9dc3ea5dd2c4f64ec7395280b9c9284
Archie Hickmott <25hickmar@gmail.com>
2024-07-17 20:12:11 +1000

    Made __init__.py more readable

--- fortifysql/__init__.py
+++ fortifysql/__init__.py
@@ -3,15 +3,15 @@ __author__ = 'Archie Hickmott'
 import sqlite3
 
 from .database import Database
-from .utils import is_always_true_where, is_drop_query, is_delete_without_where, is_dangerous_delete
-from .sql_data_types import Null, Integer, Real, Text, Blob, ALL_SQL_DATA_TYPE_NAMES, ALL_SQL_DATA_TYPES
-from .database_map import Table
+from .sql_data_types import Null, Integer, Real, Text, Blob, \
+                            ALL_SQL_DATA_TYPE_NAMES, ALL_SQL_DATA_TYPES
+from .database_map import Table, Column
 
 import sqlparse
 
-print(f"\033[93mWARNING FortifySQL is in BETA {__version__}, do not use in a production environment until full release \033[0m")
+print(f"\033[93mWARNING FortifySQL is in BETA {__version__}, 
+      do not use in a production environment until full release \033[0m")
 
-__all__ = ['Database', "Table", "column",
-           "is_always_true_where", "is_drop_query",
-           "is_delete_without_where", "is_dangerous_delete",
-           "sqlite3", "Null", "Integer", "Real", "Text", "Blob"]
+__all__ = ['Database', "Table", "column", 
+           "sqlite3", "sqlparse",
+           "Null", "Integer", "Real", "Text", "Blob", "ALL_SQL_DATA_TYPE_NAMES", "ALL_SQL_DATA_TYPES"]
